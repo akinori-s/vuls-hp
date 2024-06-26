adbab40560cb5a9906ea6f830cd1678e2e4be4c0
akinori-s <sasada.a30@gmail.com>
2024-06-26 14:05:06 +0900

    fix: vite config base path for github pages deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "/vuls-hp/",
     plugins: [react()],
     resolve: {
         alias: {
