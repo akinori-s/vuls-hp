b4768bcee08b07b500fd180e82c2844dfb69b031
akinori-s <sasada.a30@gmail.com>
2024-06-27 23:53:58 +0900

    fix: text styling for mobile

--- src/App.tsx
+++ src/App.tsx
@@ -22,8 +22,13 @@ function App() {
 				<div className="flex justify-between gap-12 lg:gap-20 max-w-7xl mx-auto px-4 py-3 md:pt-5">
 					<a href="/#" className='flex gap-2 items-center hover:bg-base-200 rounded'>
 						<img src="vuls.svg" alt="ロゴ" className="h-9 md:mr-2" />
-						<div>
-							<h1 className='text-lg leading-4 md:text-2xl md:leading-6 title2'>Future<span className='font-bold text-gradient bg-gradient-to-bl from-[#FFCE15] to-[#FF9900]'>Vuls</span></h1>
+						<div className='flex flex-row items-center h-full my-auto'>
+							<h1 className='inline text-xl md:text-2xl'>
+								Future
+							</h1>
+							<span className='inline text-xl md:text-2xl font-bold text-gradient bg-gradient-to-bl from-[#FFCE15] to-[#FF9900]'>
+								Vuls
+							</span>
 						</div>
 					</a>
 					<div className="flex-1 hidden md:flex items-end justify-start gap-4 lg:gap-12 text-md">
@@ -100,14 +105,14 @@ function App() {
 								<CarouselItem className="basis-5/6 md:basis-1/3">
 									<Card>
 										<CardHeader>
-											<CardTitle className='h-8 flex flex-row items-center mx-auto text-lg'>
-												<img className='max-w-full shrink object-contain' src="./jtb.png" alt="株式会社JTB" />
-												<h4 className='flex-none ml-3 text-[#333333] whitespace-nowrap'>株式会社JTB 様</h4>
+											<CardTitle className='h-4 md:h-8 flex flex-row items-center mx-auto text-lg'>
+												<img className='hidden md:block max-w-full shrink object-contain' src="./jtb.png" alt="株式会社JTB" />
+												<h4 className='text-lg flex-none ml-3 text-[#333333] whitespace-nowrap'>株式会社JTB 様</h4>
 											</CardTitle>
 										</CardHeader>
 										<CardContent>
 											<img className='object-contain' src="./jtb_group_image.png" alt="株式会社JTB グループ写真" />
-											<h5 className='h-16 mt-6 font-medium text-xl text-center'>
+											<h5 className='h-16 mt-6 font-medium text-md md:text-xl text-center'>
 												手作業の9割が自動化、1人月分以上の工数を削減
 											</h5>
 										</CardContent>
@@ -116,14 +121,14 @@ function App() {
 								<CarouselItem className="basis-5/6 md:basis-1/3">
 									<Card>
 										<CardHeader>
-											<CardTitle className='h-8 flex flex-row items-center mx-auto text-lg'>
-												<img className='max-w-full shrink object-contain' src="./smart_hr.png" alt="株式会社SmartHR" />
+											<CardTitle className='h-4 md:h-8 flex flex-row items-center mx-auto text-lg'>
+												<img className='hidden md:block max-w-full shrink object-contain' src="./smart_hr.png" alt="株式会社SmartHR" />
 												<h4 className='flex-none ml-3 text-[#333333] whitespace-nowrap'>株式会社SmartHR 様</h4>
 											</CardTitle>
 										</CardHeader>
 										<CardContent>
 											<img className='object-contain' src="./smart_hr_group_image.png" alt="株式会社SmartHR グループ写真" />
-											<h5 className='h-16 mt-6 font-medium text-xl text-center'>
+											<h5 className='h-16 mt-6 font-medium text-md md:text-xl text-center'>
 												まずは1台からスモールスタート、コンテナ脆弱性をSmartに統合管理
 											</h5>
 										</CardContent>
@@ -132,14 +137,14 @@ function App() {
 								<CarouselItem className="basis-5/6 md:basis-1/3">
 									<Card>
 										<CardHeader>
-											<CardTitle className='h-8 flex flex-row items-center mx-auto text-lg'>
-												<img className='max-w-full shrink object-contain' src="./akatsuki.png" alt="株式会社アカツキ" />
+											<CardTitle className='h-4 md:h-8 flex flex-row items-center mx-auto text-lg'>
+												<img className='hidden md:block max-w-full shrink object-contain' src="./akatsuki.png" alt="株式会社アカツキ" />
 												<h4 className='flex-none ml-3 text-[#333333] whitespace-nowrap'>株式会社アカツキ 様</h4>
 											</CardTitle>
 										</CardHeader>
 										<CardContent>
 											<img className='object-contain' src="./akatsuki_group_image.png" alt="株式会社アカツキ グループ写真" />
-											<h5 className='h-16 mt-6 font-medium text-xl text-center'>
+											<h5 className='h-16 mt-6 font-medium text-md md:text-xl text-center'>
 												セキュリティは「コスト」ではなく「投資」
 											</h5>
 										</CardContent>
@@ -148,14 +153,14 @@ function App() {
 								<CarouselItem className="basis-5/6 md:basis-1/3">
 									<Card>
 										<CardHeader>
-											<CardTitle className='h-8 flex flex-row items-center mx-auto text-lg'>
-												<img className='max-w-full shrink object-contain' src="./sompo.png" alt="損害保険ジャパン株式会社" />
+											<CardTitle className='h-4 md:h-8 flex flex-row items-center mx-auto text-lg'>
+												<img className='hidden md:block max-w-full shrink object-contain' src="./sompo.png" alt="損害保険ジャパン株式会社" />
 												<h4 className='flex-none ml-3 text-[#333333] whitespace-nowrap'>損害保険ジャパン株式会社 様</h4>
 											</CardTitle>
 										</CardHeader>
 										<CardContent>
 											<img className='object-contain' src="./sompo_group_image.png" alt="株式会社JTB グループ写真" />
-											<h5 className='h-16 mt-6 font-medium text-xl text-center'>
+											<h5 className='h-16 mt-6 font-medium text-md md:text-xl text-center'>
 												運用のレベルが1つ上がりました
 											</h5>
 										</CardContent>
@@ -164,14 +169,14 @@ function App() {
 								<CarouselItem className="basis-5/6 md:basis-1/3">
 									<Card>
 										<CardHeader>
-											<CardTitle className='h-8 flex flex-row items-center mx-auto text-lg'>
-												<img className='max-w-full shrink object-contain' src="./mainavi.png" alt="株式会社JTB" />
+											<CardTitle className='h-4 md:h-8 flex flex-row items-center mx-auto text-lg'>
+												<img className='hidden md:block max-w-full shrink object-contain' src="./mainavi.png" alt="株式会社JTB" />
 												<h4 className='flex-none ml-3 text-[#333333] whitespace-nowrap'>株式会社JTB 様</h4>
 											</CardTitle>
 										</CardHeader>
 										<CardContent>
 											<img className='object-contain' src="./mainavi_group_image.png" alt="株式会社JTB グループ写真" />
-											<h5 className='h-16 mt-6 font-medium text-xl text-center'>
+											<h5 className='h-16 mt-6 font-medium text-md md:text-xl text-center'>
 												Log4Shellのような経験はもう二度としない
 											</h5>
 										</CardContent>
@@ -212,7 +217,7 @@ function App() {
 					<div className='max-w-7xl mx-auto px-4 py-10 md:pt-20 md:pb-20'>
 						<div className='flex items-center lg:items-start justify-center flex-col lg:flex-row lg:gap-12 mb-8 md:mb-16'>
 							<div className='flex flex-col text-center w-full'>
-								<h2 className='md:text-lg font-bold text-gradient bg-gradient-to-bl from-[#FFCE15] to-[#FF9900]'>HOW IT WORKS</h2>
+								<h2 className='text-xs md:text-lg font-bold text-gradient bg-gradient-to-bl from-[#FFCE15] to-[#FF9900]'>HOW IT WORKS</h2>
 								<h3 className='text-lg md:text-3xl font-bold text-[#333333] text-center'>FutureVulsの仕組み</h3>
 							</div>
 						</div>
@@ -247,14 +252,14 @@ function App() {
 					<div className='max-w-7xl mx-auto px-4 py-10 md:pt-20 md:pb-20'>
 						<div className='flex items-center lg:items-start justify-center flex-col lg:flex-row lg:gap-12 mb-8 md:mb-16'>
 							<div className='flex flex-col text-center w-full'>
-								<h2 className='md:text-lg font-bold text-gradient bg-gradient-to-bl from-[#FFCE15] to-[#FF9900]'>FEATURES</h2>
+								<h2 className='text-xs md:text-lg font-bold text-gradient bg-gradient-to-bl from-[#FFCE15] to-[#FF9900]'>FEATURES</h2>
 								<h3 className='text-lg md:text-3xl font-bold text-[#333333] text-center'>特徴</h3>
 							</div>
 						</div>
 						<div className='w-full flex flex-col gap-10 px-3 md:px-0'>
 							<div className='flex flex-col md:flex-row md:gap-10'>
-								<div className='md:w-1/2 bg-white rounded-xl py-6 md:py-0'>
-									<h3 className='md:text-md font-bold text-gradient bg-gradient-to-bl from-[#FFCE15] to-[#FF9900] mb-1'>1 SSVCモデルを使用した優先度付け</h3>
+								<div className='md:w-1/2 bg-white rounded-xl pb-6 md:py-0'>
+									<h3 className='text-sm md:text-md font-bold text-gradient bg-gradient-to-bl from-[#FFCE15] to-[#FF9900] mb-1'>1 SSVCモデルを使用した優先度付け</h3>
 									<h4 className='text-xl font-bold mb-4'>脆弱性スキャナとしての高い支持</h4>
 									<p className='text-md mb-4'>
 										年間2万件以上の脆弱性が新たに公開されています
@@ -269,8 +274,8 @@ function App() {
 								</div>
 							</div>
 							<div className='flex flex-col md:flex-row-reverse md:gap-10'>
-								<div className='md:w-1/2 bg-white rounded-xl py-6 md:py-0'>
-									<h3 className='md:text-md font-bold text-gradient bg-gradient-to-bl from-[#FFCE15] to-[#FF9900] mb-1'>2 脆弱性スキャナとしての高い支持</h3>
+								<div className='md:w-1/2 bg-white rounded-xl pb-6 md:py-0'>
+									<h3 className='text-sm md:text-md font-bold text-gradient bg-gradient-to-bl from-[#FFCE15] to-[#FF9900] mb-1'>2 脆弱性スキャナとしての高い支持</h3>
 									<h4 className='text-xl font-bold mb-4'>脆弱性スキャナとしての高い支持</h4>
 									<p className='text-md mb-4'>
 										年間2万件以上の脆弱性が新たに公開されています
@@ -285,8 +290,8 @@ function App() {
 								</div>
 							</div>
 							<div className='flex flex-col md:flex-row md:gap-10'>
-								<div className='md:w-1/2 bg-white rounded-xl py-6 md:py-0'>
-									<h3 className='md:text-md font-bold text-gradient bg-gradient-to-bl from-[#FFCE15] to-[#FF9900] mb-1'>3 脆弱性対応フローの自動化</h3>
+								<div className='md:w-1/2 bg-white rounded-xl pb-6 md:py-0'>
+									<h3 className='text-sm md:text-md font-bold text-gradient bg-gradient-to-bl from-[#FFCE15] to-[#FF9900] mb-1'>3 脆弱性対応フローの自動化</h3>
 									<h4 className='text-xl font-bold mb-4'>脆弱性スキャナとしての高い支持</h4>
 									<p className='text-md mb-4'>
 										年間2万件以上の脆弱性が新たに公開されています
@@ -307,7 +312,7 @@ function App() {
 					<div className='max-w-7xl mx-auto px-4 py-10 md:pt-20 md:pb-20'>
 						<div className='flex items-center lg:items-start justify-center flex-col lg:flex-row lg:gap-12 mb-8 md:mb-16'>
 							<div className='flex flex-col text-center w-full'>
-								<h2 className='md:text-lg font-bold text-gradient bg-gradient-to-bl from-[#FFCE15] to-[#FF9900]'>PRICING</h2>
+								<h2 className='text-xs md:text-lg font-bold text-gradient bg-gradient-to-bl from-[#FFCE15] to-[#FF9900]'>PRICING</h2>
 								<h3 className='text-lg md:text-3xl font-bold text-[#333333] text-center'>料金</h3>
 							</div>
 						</div>
@@ -316,11 +321,11 @@ function App() {
 								{/* Left Column */}
 								<div className="bg-white p-8 rounded-lg md:rounded-none md:rounded-l-xl shadow-md flex flex-col justify-between items-center">
 									<div className='w-full'>
-										<h3 className='md:text-md font-bold text-gradient bg-gradient-to-bl from-[#FFCE15] to-[#FF9900]'>OSS Vuls</h3>
-										<h4 className='text-xl font-bold mb-4'>オープンソースVuls</h4>
-										<p className='mb-4 text-[#333333]'>脆弱性をスキャン</p>
+										<h3 className='text-sm md:text-md font-bold text-gradient bg-gradient-to-bl from-[#FFCE15] to-[#FF9900]'>OSS Vuls</h3>
+										<h4 className='text-lg md:text-xl font-bold mb-2 md:mb-4'>オープンソースVuls</h4>
+										<p className='mb-2 md:mb-4 text-[#333333]'>脆弱性をスキャン</p>
 										<p className='text-lg md:text-2xl text-[#333333] font-bold tracking-tighter pb-8 md:pb-10'>
-											<span className='text-4xl font-black'>無料</span>
+											<span className='text-3xl md:text-4xl font-black'>無料</span>
 										</p>
 										<ul className='text-md text-[#333333] pb-10 md:pb-12'>
 											<li className='flex items-center justify-start lg:justify-start gap-1.5 pb-1 mb-1'>
@@ -351,11 +356,11 @@ function App() {
 								{/* Middle Column */}
 								<div className="bg-white p-8 shadow-md flex flex-col justify-between items-center">
 									<div className='w-full'>
-										<h3 className='md:text-md font-bold text-gradient bg-gradient-to-bl from-[#FFCE15] to-[#FF9900]'>Standard</h3>
-										<h4 className='text-xl font-bold mb-4'>FutureVuls Standard</h4>
-										<p className='mb-4 text-[#333333]'>脆弱性を管理</p>
+										<h3 className='text-sm md:text-md font-bold text-gradient bg-gradient-to-bl from-[#FFCE15] to-[#FF9900]'>Standard</h3>
+										<h4 className='text-lg md:text-xl font-bold mb-2 md:mb-4'>FutureVuls Standard</h4>
+										<p className='mb-2 md:mb-4 text-[#333333]'>脆弱性を管理</p>
 										<p className='text-lg md:text-xl text-[#333333] font-bold tracking-tighter pb-8 md:pb-10'>
-											<span className='text-4xl font-black'>4,000</span>円より
+											<span className='text-3xl md:text-4xl font-black'>4,000</span>円より
 										</p>
 										<ul className='text-md text-[#333333] pb-10 md:pb-12'>
 											<li className='flex items-center justify-start lg:justify-start gap-1.5 pb-1 mb-1'>
@@ -392,11 +397,11 @@ function App() {
 								{/* Right Column */}
 								<div className="bg-white p-8 rounded-lg md:rounded-none md:rounded-r-xl shadow-md flex flex-col justify-between items-center">
 									<div className='w-full'>
-										<h3 className='md:text-md font-bold text-gradient bg-gradient-to-bl from-[#FFCE15] to-[#FF9900]'>CSIRT</h3>
-										<h4 className='text-xl font-bold mb-4'>FutureVuls CSIRT</h4>
-										<p className='mb-4 text-[#333333]'>全社の脆弱性を横断管理</p>
+										<h3 className='text-sm md:text-md font-bold text-gradient bg-gradient-to-bl from-[#FFCE15] to-[#FF9900]'>CSIRT</h3>
+										<h4 className='text-lg md:text-xl font-bold mb-2 md:mb-4'>FutureVuls CSIRT</h4>
+										<p className='mb-2 md:mb-4 text-[#333333]'>全社の脆弱性を横断管理</p>
 										<p className='text-lg md:text-2xl text-[#333333] font-bold tracking-tighter pb-8 md:pb-10'>
-											<span className='text-4xl font-black'>要見積</span>
+											<span className='text-3xl md:text-4xl font-black'>要見積</span>
 										</p>
 										<ul className='text-md text-[#333333] pb-10 md:pb-12'>
 											<li className='flex items-center justify-start lg:justify-start gap-1.5 pb-1 mb-1'>
@@ -530,25 +535,25 @@ function App() {
 						<div className='col-span-1 text-sm text-center md:text-left mb-4 md:mb-0'>
 							<h5 className='text-[#333333] mb-2'>サービス紹介</h5>
 							<ul className='text-[#828282] leading-relaxed md:leading-loose'>
-								<li>導入事例</li>
-								<li>特徴</li>
-								<li>FutureVulsの仕組み</li>
-								<li>セキュリティコンサルティング</li>
+								<li className='hover:underline'>導入事例</li>
+								<li className='hover:underline'>特徴</li>
+								<li className='hover:underline'>FutureVulsの仕組み</li>
+								<li className='hover:underline'>セキュリティコンサルティング</li>
 							</ul>
 						</div>
 						<div className='col-span-1 text-sm text-center md:text-left mb-4 md:mb-0'>
 							<h5 className='text-[#333333] mb-2'>ドキュメント</h5>
 							<ul className='text-[#828282] leading-relaxed md:leading-loose'>
-								<li>マニュアル</li>
-								<li>リリースノート</li>
-								<li>ヘルプ</li>
+								<li className='hover:underline'>マニュアル</li>
+								<li className='hover:underline'>リリースノート</li>
+								<li className='hover:underline'>ヘルプ</li>
 							</ul>
 						</div>
 						<div className='col-span-1 text-sm text-center md:text-left mb-4 md:mb-0'>
 							<h5 className='text-[#333333] mb-2'>SNS</h5>
 							<ul className='text-[#828282] leading-relaxed md:leading-loose'>
-								<li>X</li>
-								<li>ブログ</li>
+								<li className='hover:underline'>X</li>
+								<li className='hover:underline'>ブログ</li>
 							</ul>
 						</div>
 					</div>
